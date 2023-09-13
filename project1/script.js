13cdb38496e6244e4eec3d416a9cf15df9300030
bmalvo <boydmalvo@gmail.com>
2023-09-13 20:32:30 +0200

    create font-family selector

--- project1/script.js
+++ project1/script.js
@@ -1,9 +1,14 @@
 addEventListener('DOMContentLoaded', () => {
     const rangeInput = document.querySelector('.rangeInput');
     const sampleText = document.querySelector('.sampleText');
+    const fontSelector = document.querySelector('.fontSelector'); 
 
     rangeInput.addEventListener('input', function() {
         sampleText.style.fontSize = this.value + 'px';
         console.log(this.value)
+    });
+
+    fontSelector.addEventListener('change', () => {
+        sampleText.style.fontFamily = fontSelector.value;
     });
 });
