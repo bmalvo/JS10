6a15f0592d23466624dd33fdebdf11a75b9c5d91
bmalvo <boydmalvo@gmail.com>
2023-09-29 20:50:46 +0200

    project6

---
+++ project6/script.js
@@ -0,0 +1,31 @@
+const getFace = async (e) => {
+    e.preventDefault()
+    const gender = document.getElementById('gender').value;
+    const age = document.getElementById('age').value;
+    const ethnicity = document.getElementById('ethnicity').value;
+    
+    const url = `https://face-studio.p.rapidapi.com/generate?gender=${gender}&age=${age}s&ethnicity=${ethnicity}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': 'a8d4d52867msh686f0720d16844dp135c14jsn3fdb3397eafb',
+            'X-RapidAPI-Host': 'face-studio.p.rapidapi.com'
+        }
+    };
+    
+    try {
+        const response = await fetch(url, options);
+        const result = await response.blob();
+        const image = URL.createObjectURL(result);
+        const imageDOM = document.createElement('img');
+        const parDOM = document.createElement('p');
+        parDOM.textContent = url;
+        imageDOM.src = image;
+        document.body.appendChild(parDOM);
+        document.body.appendChild(imageDOM);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+document.querySelector('#form').addEventListener('submit', getFace)
