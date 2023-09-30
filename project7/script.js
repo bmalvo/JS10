27afc153e41c180f436baa1c979e37bd6d1fc630
bmalvo <boydmalvo@gmail.com>
2023-09-30 22:33:33 +0200

    project 7 end

--- project7/script.js
+++ project7/script.js
@@ -1,7 +1,7 @@
 const questionData = [
     ['Które z tych imion jest brzydkie?', ['Kunegunda', 'Helga', 'Wanda'], 0],
     ['Z kim graniczy Polska?', ['z Madagaskarem', 'z Litwą'], 1],
-    ['Który kot jest najładniejszy?', ['Ninja, Brydzia, Stefka', 2]],
+    ['Który kot jest najładniejszy?', ['Ninja', 'Brydzia', 'Stefka'], 2],
 ];
 
 
@@ -21,6 +21,14 @@ class Question {
     get title() {
         return this.#title;
     }
+
+    get answers() {
+        return this.#answers
+    }
+
+    get correctAnswer() {
+        return this.#correctAnswer
+    }
  }
 
 class Quiz {
@@ -31,7 +39,6 @@ class Quiz {
 
     constructor(questions) {
         this.#questions = questions.map(question => new Question(question[0], question[1], question[2]));
-        console.log(this.#questions);
         this.#currentQuestionIndex = 0;
         this.#score = 0;
     }
@@ -40,8 +47,44 @@ class Quiz {
         
         const questionElement = document.getElementById('question');
         const answersElement = document.getElementById('answers');
+        answersElement.textContent = '';
 
-        questionElement.textContent = this.#questions[this.#currentQuestionIndex].title
+        questionElement.textContent = this.#questions[this.#currentQuestionIndex].title;
+        this.#questions[this.#currentQuestionIndex].answers.forEach((answer, index) => {
+            // console.log(answer);
+            const answerElement = document.createElement('li');
+            answerElement.innerHTML = `<label><input type='radio' value=${index} name='answer'> ${answer}</label> `
+            answersElement.appendChild(answerElement); 
+        });
+    }
+
+    nextQuestion() {
+        const selectedAnswer = document.querySelector('input[name="answer"]:checked');
+        console.log(selectedAnswer);
+
+        if (selectedAnswer) {
+            console.log(selectedAnswer.value)
+            if (parseInt(selectedAnswer.value) === this.#questions[this.#currentQuestionIndex].correctAnswer) {
+                console.log('Brawo!');
+                this.#score++;
+            } else {
+                console.log('Zła odpowiedź')
+            }
+            this.#currentQuestionIndex++;
+            if (this.#currentQuestionIndex < this.#questions.length) {
+                this.displayQuestion();
+            } else {
+                // alert(`Koniec. Zdobyłeś ${this.#score}  na ${this.#questions.length}`);
+                this.displayResult();
+            }
+        } else {
+            alert('Nie udzieliłeś odpowiedzi')
+        }
+    }
+
+    displayResult() {
+        const resultElement = document.getElementById('result');
+        resultElement.textContent = `Koniec. Zdobyłeś ${this.#score} na ${this.#questions.length}`;
     }
 }
 
