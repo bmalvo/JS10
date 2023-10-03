cd5dae676f65b45e9bf8c38d4f6c72bd27a34a63
bmalvo <boydmalvo@gmail.com>
2023-10-03 18:08:49 +0200

    project8 end

--- project8/script.js
+++ project8/script.js
@@ -11,29 +11,50 @@ class DrawingApp {
         this.canvas.height = window.innerHeight;
         this.ctx = this.canvas.getContext('2d');
         this.ctx.strokeStyle = 'white';
+        this.ctx.lineWitdh = 50;
+        this.ctx.lineCap = 'round';
     }
 
     #initEvents() {
         //destop methods
+        this.canvas.addEventListener('mousedown', (e) => {
+            this.#startPosition(e);
+        });
+        this.canvas.addEventListener('mouseup', this.#endPosition.bind(this));
+        this.canvas.addEventListener('mousemove', this.#draw.bind(this));
+
         //mobile methods
+        this.canvas.addEventListener('touchstart', (e) => {
+            this.#startPosition(e);
+        });
+        this.canvas.addEventListener('touchend', this.#endPosition.bind(this));
+        this.canvas.addEventListener('touchmove', this.#draw.bind(this));
     }
 
-    #startPosition() {
+    #startPosition(e) {
         this.painting = true;
-        this.#draw();
+        this.#draw(e);
     }
 
     #endPosition() {
         this.painting = false;
+        this.ctx.beginPath();
+
     }
 
-    #draw() {
-        // s
+    #draw(e) {
+        if (!this.painting) return; 
+
+        let x = e.clientX || e.touches[0].clientX;
+        let y = e.clientY || e.touches[0].clientY;
+
+        this.ctx.lineTo(x, y);
+        this.ctx.stroke();
+        this.ctx.moveTo(x, y);
     }
 
     changeColor(color) {
         this.ctx.strokeStyle = color;
-        console.log(color);
     }
 }
 
